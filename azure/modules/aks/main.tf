29f2d2a8c2a05863433012cc53bb916ea6c85203
rodolfossouza <rodolfo.ti@outlook.com>
2023-03-11 20:33:45 -0300

    updating aks module

--- azure/modules/aks/main.tf
+++ azure/modules/aks/main.tf
@@ -1,3 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.41.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+  required_version = ">=1.3.7"
+}
+
+provider "azurerm" {
+    features {}
+    subscription_id = var.subscription_id 
+    tenant_id = var.tenant_id
+  # Configuration options
+}
+
 # Create a Resource Group for Resources
 resource "azurerm_resource_group" "rodnet-aks-rg" {
   name     = "${var.prefix}-rg"
